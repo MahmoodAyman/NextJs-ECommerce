4c6ea8de9d2527985eeb48a79b22a175fa95dafa
MahmoodAyman <mahmoudayman1542@gmail.com>
2025-02-05 11:42:08 +0200

    remove setLoading from Loader

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,12 +1,9 @@
 "use client";
-import { useState } from "react";
 import { BeatLoader } from "react-spinners";
 const LoadingPage = () => {
-  const [loading, setLoading] = useState(false);
   return (
     <div className="sweet-loading h-screen flex-center">
       <BeatLoader
-        loading={loading}
         color="#FFCA28"
         size={15}
         aria-label="loading spineer"
