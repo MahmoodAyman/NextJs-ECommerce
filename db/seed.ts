285bb4b1007415a5aaeb0c493386ef66320b1c31
MahmoodAyman <mahmoudayman1542@gmail.com>
2025-02-08 21:32:27 +0200

    error in seed.ts file

--- db/seed.ts
+++ db/seed.ts
@@ -5,12 +5,12 @@ import sampleData from "./sample-data";
 async function main() {
   const prisma = new PrismaClient();
   await prisma.product.deleteMany();
-  await prisma.Account.deleteMany();
-  await prisma.Session.deleteMany();
-  await prisma.Verification.deleteMany();
-  await prisma.User.deleteMany();
+  await prisma.account.deleteMany();
+  await prisma.session.deleteMany();
+  await prisma.verification.deleteMany();
+  await prisma.user.deleteMany();
 
-  await prisma.User.createMany({ data: sampleData.users });
+  await prisma.user.createMany({ data: sampleData.users });
   await prisma.product.createMany({ data: sampleData.products });
   console.log("Data base seeded successfully");
 }
